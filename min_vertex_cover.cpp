26c84a6458adb2229a3ed63dcb2ef701538b670c
Diego Iturra <diturra@dparadig.com>
2022-07-10 18:25:55 -0400

    2-aprox vertex cover implementado

--- min_vertex_cover.cpp
+++ min_vertex_cover.cpp
@@ -6,50 +6,41 @@
 
 using namespace std;
 
-struct hashFunction{
-	bool operator()(const pair<int,int>& other) const{
-		return other.first && other.second;
-	}
-};
 
 //Nodos van desde posicion [0...N-1] para simplicidad del ejercicio
 class Graph{
 	private:
 		int numVertex;
-		vector<unordered_set<int> > graph;
-		unordered_set< pair<int,int> , hashFunction> setOfEdges;
+		vector<unordered_set<int> > graph; //lista de adyacencia
+		vector<bool> visited;
+
+
+		//Verifica que todas las incidencias esten vacias , si hay al menos una que contenga un vertice
+		//Entonces devolver false
+		bool isEmpty(vector<unordered_set<int> >& edges){
+			for(int i=0 ; i<edges.size() ; i++){
+				if(!edges[i].empty()){
+					return false;
+				}
+			}
+			return true;
+		}
 
 	public:
 		Graph(const int numVertex){
 			this->numVertex = numVertex;
 			this->graph.resize(numVertex);
+			this->visited.resize(numVertex,false);
 		}
+
 
 		void addEdge(const int u, const int v){
 			if(u == v || u >= numVertex || v >= numVertex || u < 0 || v < 0)
 				return;
 			this->graph[u].insert(v);
 			this->graph[v].insert(u);
-			this->setOfEdges.insert(make_pair(u,v));
-			this->setOfEdges.insert(make_pair(v,u));
 		}
 
-		void removeEdge(int u , int v){
-			if(u == v || u >= numVertex || v >= numVertex || u < 0 || v < 0)
-				return;
-			this->graph[u].erase(v);
-			this->graph[v].erase(u);
-			this->setOfEdges.erase(make_pair(u,v));
-			this->setOfEdges.erase(make_pair(v,u));
-		}
-
-		void printEdges(){
-			unordered_set<pair<int,int> , hashFunction>::iterator it;
-			for(it = setOfEdges.begin() ; it != setOfEdges.end() ; it++){
-				cout << "(" << it->first << "," << it->second << ")" << endl;
-			}
-			cout << endl;
-		}
 
 		void printGraph(){
 			unordered_set<int>::iterator it;
@@ -62,26 +53,88 @@ class Graph{
 			}
 			cout << endl;
 		}
+
+		void aproxVertexCover(){
+			unordered_set<int> coveredSet; //C
+			vector<unordered_set<int> > uncoveredEdges = this->graph; //E
+
+			while(!isEmpty(uncoveredEdges)){
+				//Tomar una arista cualquiera (u,v) de uncoveredEdges
+				int u,v;
+				for(int i=0 ; i<numVertex ; i++){
+					if(!visited[i]){
+						u = i;
+						visited[i] = true;
+
+						unordered_set<int>::iterator it = graph[i].begin();
+						v = *it;
+						visited[v] = true;
+						break;
+					}
+				}
+
+				cout << "Arista tomada: (" << u << "," << v << ")" << endl;
+
+
+				//insertar arista (u,v) en coveredSet y eliminarla de uncoveredEdges
+				coveredSet.insert(u);
+				coveredSet.insert(v);
+				uncoveredEdges[u].clear();
+				uncoveredEdges[v].clear(); //Elimina adyacencia directa desde los vertices u,v
+
+				//remover aristas incidentes tanto como a 'u' y a 'v' de uncoveredEdges
+				for(int i=0 ; i<numVertex ; i++){
+					if(!visited[i]){
+						unordered_set<int>::iterator it;
+						for(it = graph[i].begin() ; it != graph[i].end() ; it++){
+							if(visited[*it]){
+								uncoveredEdges[i].erase(*it);
+							}
+						}
+					}
+				}
+
+			}
+
+			cout << "Numero de vertices en 2-aprox vertex cover: " << coveredSet.size() << endl;
+
+			for(unordered_set<int>::iterator it = coveredSet.begin() ; it != coveredSet.end() ; it++){
+				cout << *it << " ";
+			}
+			cout << endl;
+		}
 };
 
 
 
 int main(){
 
-	int numVertex = 5;
+	int numVertex = 7;
+	
+	//Ejemplo con vertex cover 2-aproximado = 6 y valor optimo = 3
 	Graph graph = Graph(numVertex);
-
 	graph.addEdge(0,1);
-	graph.addEdge(0,2);
-	graph.addEdge(0,3);
-	graph.addEdge(0,4);
 	graph.addEdge(1,2);
 	graph.addEdge(2,3);
+	graph.addEdge(2,4);
 	graph.addEdge(3,4);
+	graph.addEdge(4,5);
+	graph.addEdge(3,6);
 
 	graph.printGraph();
-	graph.printEdges();
+	graph.aproxVertexCover();
 
+
+	//Ejemplo en el cual vertex cover 2-aproximado = optimo con numero de vertices igual a 2
+	numVertex = 6;
+	Graph graph2 = Graph(numVertex);
+	graph2.addEdge(0,1);
+	graph2.addEdge(0,2);
+	graph2.addEdge(0,3);
+	graph2.addEdge(0,4);
+	graph2.addEdge(0,5);
+	graph2.printGraph();
+	graph2.aproxVertexCover();
 
 	return 0;
 }
